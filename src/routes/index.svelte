69bb4b06d0957e8cf6d09cd3c4ff4733afbb584a
Christoforos Charalambous <chrischaralambous14@gmail.com>
2022-06-10 03:27:19 +0300

    Remove target="_blank"

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
 </div>
 <div class="text">
     <p>Hello! My name is Chris Charalambous.</p>
-    <p>I'm currently studying <a href="https://www.ece.ntua.gr/en" target="_blank" rel="noopener noreferrer">Electrical & Computer Engineering</a> at the <a href="https://ntua.gr/en/" target="_blank" rel="noopener noreferrer">National Technical University of Athens</a>.</p>
+    <p>I'm currently studying <a href="https://www.ece.ntua.gr/en">Electrical & Computer Engineering</a> at the <a href="https://ntua.gr/en/">National Technical University of Athens</a>.</p>
     <p>I've started programming at a young age and I've learned a decent level:</p>
     <ul>
         <li>Golang, Python, C++, Javascript, HTML, CSS</li>
@@ -24,19 +24,19 @@
 <p>Best way to contact me is through email: <a href="mailto:me@chrisch.dev">me@chrisch.dev</a></p>
 <p>But you can also find me here:</p>
 <div class="iconList">
-    <a class="zero" href="https://github.com/ttofis" target="_blank" rel="noopener noreferrer"><div class="single">
+    <a class="zero" href="https://github.com/ttofis"><div class="single">
         <Icon style="font-size: 5em" icon="akar-icons:github-fill" />
         <p>github</p>
     </div></a>
-    <a class="zero" href="https://twitter.com/elchris414" target="_blank" rel="noopener noreferrer"><div class="single">
+    <a class="zero" href="https://twitter.com/elchris414"><div class="single">
         <Icon style="font-size: 5em" icon="akar-icons:twitter-fill" />
         <p>twitter</p>
     </div></a>
-    <a class="zero" href="https://linkedin.com/in/chrischaralambous14/" target="_blank" rel="noopener noreferrer"><div class="single">
+    <a class="zero" href="https://linkedin.com/in/chrischaralambous14/"><div class="single">
         <Icon style="font-size: 5em" icon="akar-icons:linkedin-fill" />
         <p>linkedin</p>
     </div></a>
-    <a class="zero" href="https://discordapp.com/users/125228190825316352" target="_blank" rel="noopener noreferrer"><div class="single">
+    <a class="zero" href="https://discordapp.com/users/125228190825316352"><div class="single">
         <Icon style="font-size: 5em" icon="akar-icons:discord-fill" />
         <p>discord</p>
     </div></a>
