b7f18d00018de9a8c30aebad84e139da6b229130
Christoforos Charalambous <chrischaralambous14@gmail.com>
2022-06-14 22:13:42 +0300

    Remove from svelte:head

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,33 +17,32 @@
 	<meta name="description" content="Personal website of Chris Charalambous" />
     <meta name="twitter:description" content="Personal website of Chris Charalambous" />
     <meta name="og:description" content="Personal website of Chris Charalambous" />
-    {#if theme==="dark"}
-        <style>
-            :root {
-                --headerColor: rgba(18,18,18,0.8);
-                --ffHeaderColor: #121212;
-                --linkColor: #03da9d;
-                --textColor: white;
-                --borderColor: white;
-                background-color: #121212;
-            }
-        </style>
-    {:else}
-        <style>
-            :root {
-                --headerColor: rgba(255,255,255,0.8);
-                --ffHeaderColor: white;
-                --linkColor: blue;
-                --textColor: black;
-                --borderColor: #222;
-                background-color: white;
-                
-            }
-        </style>
-    {/if}
 </svelte:head>
 
 <DarkMode bind:theme />
+{#if theme==="dark"}
+    <style>
+        :root {
+            --headerColor: rgba(18,18,18,0.8);
+            --ffHeaderColor: #121212;
+            --linkColor: #03da9d;
+            --textColor: white;
+            --borderColor: white;
+            background-color: #121212;
+        }
+    </style>
+{:else}
+    <style>
+        :root {
+            --headerColor: rgba(255,255,255,0.8);
+            --ffHeaderColor: white;
+            --linkColor: blue;
+            --textColor: black;
+            --borderColor: #222;
+            background-color: white;
+        }
+    </style>
+{/if}
 
 <div>
     <Header />
