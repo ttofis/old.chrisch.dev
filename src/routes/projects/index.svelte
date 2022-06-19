028c72679e2925319c25fc753dda9e16152bb7b6
Christoforos Charalambous <chrischaralambous14@gmail.com>
2022-06-19 23:37:04 +0300

    Project revamp Still needs work

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -14,50 +14,84 @@
 <h1>Projects</h1>
 <p>Here are most of my finished projects</p>
 
-<div class="grid">
+<div class="list">
     <div class="item">
         <h2>chrisch.dev</h2>
-        <p>The site you're currently viewing.</p>
-        <div class="bottom">
-            <p><b>Technologies used: </b>Sveltekit, Netlify</p>
-            <div class="icons">
-                <a href="/posts/firstpost"><Icon icon="akar-icons:info-fill" /></a>
-                <a href="/"><Icon class="icon" icon="akar-icons:link-chain" /></a>
-                <a href="https://github.com/ttofis/chrisch.dev"><Icon class="icon" icon="akar-icons:github-fill" /></a>
+        <div class="inner">
+            <img src="/projects/chrisch.dev.png" alt="Crosswordle game" />
+            <div class="flexy">
+                <p>The site you're currently viewing.</p>
+                <div class="bottom">
+                    <p><b>Technologies used: </b>Node, Svelte, Sveltekit, Netlify</p>
+                    <div class="icons">
+                        <a href="/posts/firstpost"><Icon icon="akar-icons:info-fill" /></a>
+                        <a href="/"><Icon class="icon" icon="akar-icons:link-chain" /></a>
+                        <a href="https://github.com/ttofis/chrisch.dev"><Icon class="icon" icon="akar-icons:github-fill" /></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     <div class="item">
         <h2>Crosswordle</h2>
-        <p>A wordle inspired game crossword twist!</p>
-        <div class="bottom">
-            <p><b>Technologies used: </b>Svelte, Netlify</p>
-            <div class="icons">
-                <a href="https://crosswordle.chrisch.dev"><Icon class="icon" icon="akar-icons:link-chain" /></a>
+        <div class="inner">
+            <img src="/projects/crosswordle.png" alt="Crosswordle game" />
+            <div class="flexy">
+                <p>A wordle inspired game crossword twist!</p>
+                <div class="bottom">
+                    <p><b>Technologies used: </b>Node, Svelte, Netlify</p>
+                    <div class="icons">
+                        <a href="https://crosswordle.chrisch.dev"><Icon class="icon" icon="akar-icons:link-chain" /></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 <style>
-    .grid {
-        width: 100%;
-        display: grid;
-        grid-template-columns: calc(50% - 10px) calc(50% - 10px);
-        grid-gap: 20px;
-    }
-    .item {
-        border: 2px var(--borderColor) solid;
-        padding: 10px;
-        border-radius: 10px;
+    .flexy {
         display: flex;
         flex-direction: column;
+        width: 100%;
+    }
+    .inner {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row-reverse;
+    }
+    .item img {
+        margin-top: 10px;
+        width: 60%;
+        aspect-ratio: 0;
+    }
+    @media only screen and (max-width: 600px) {
+        .inner {
+            flex-direction: column;
+            justify-content: baseline;
+        }
+        .item img {
+            width: 100%;
+            aspect-ratio: 0;
+        }
+    }    
+    .list {
+        width: 100%;
+        padding: 0;
+    }
+    .item {
+        margin-bottom: 30px;
+        /*border: 2px var(--borderColor) solid;
+        padding: 10px;*/
+        width: auto;
+        border-radius: 10px;
     }
     .icons {
         font-size: 2em;
     }
     .item h2 {
-        width:100%;
+        width: 100%;
         border-bottom: 2px var(--borderColor) solid;
         margin: 0;
         font-size: min(5vw, 1.5em);
