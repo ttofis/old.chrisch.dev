0968a045f3045c4712be5054532d28a3f742ddd2
Christoforos Charalambous <chrischaralambous14@gmail.com>
2023-02-23 16:25:11 +0200

    Proper error page

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 
-export const theme = writable("light");
+export const theme = writable("dark");
 
 if (browser) {
     const storedTheme = localStorage.getItem("theme");
---
+++ src/routes/+error.svelte
@@ -0,0 +1,71 @@
+<script context="module">
+    import { page } from '$app/stores';
+</script>
+
+<script>
+    import '../app.css';
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+    import { theme } from "$lib/theme";
+
+    let bgColor = $theme === "dark" ? "#121212" : "#FFFFFF";
+    
+    $: {
+        bgColor = $theme === "dark" ? "#121212" : "#FFFFFF";
+    }
+</script>
+
+<svelte:head>
+    <title>{$page.status} - chrisch</title>
+	<meta name="description" content="Personal website of Chris Charalambous" />
+    <meta name="og:description" content="Personal website of Chris Charalambous" />
+    <meta name="theme-color" content={bgColor}>
+</svelte:head>
+
+{#if $theme==="dark"}
+    <style>
+        :root {
+            --headerColor: rgba(18,18,18,0.8);
+            --ffHeaderColor: #121212;
+            --linkColor: #03da9d;
+            --textColor: white;
+            --borderColor: white;
+            background-color: #121212;
+        }
+    </style>
+{:else}
+    <style>
+        :root {
+            --headerColor: rgba(255,255,255,0.8);
+            --ffHeaderColor: white;
+            --linkColor: blue;
+            --textColor: black;
+            --borderColor: #222;
+            background-color: white;
+        }
+    </style>
+{/if}
+
+<div>
+    <Header />
+    <div class="align">
+        <main>
+            <h1>{$page.status}</h1>
+            <p>Something went wrong!</p>
+        </main>
+
+        <Footer />
+    </div>
+</div>
+
+<style>
+    h1 {
+        text-align: center;
+        font-size: 10vw;
+        margin-top: 25px;
+        margin-bottom: 25px;
+    }
+    p {
+        text-align: center;
+    }
+</style>
